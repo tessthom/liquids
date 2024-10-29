457747ebe3b041509b87ed6e5d52cebbe9943482
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2024-10-29 18:51:46 -0400

    Add organizational wrapper fields to post schema

--- api/models/Post.js
+++ api/models/Post.js
@@ -25,22 +25,32 @@ const { Schema } = mongoose;
 const postSchema = new Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
-  title: { 
-    type: String, 
-    required: true,
-    maxlength: 80
-  },
-  description: { type: String },
-  imageUrl: { type: String },
-  externalLink: { type: String },
-  ingredients: [{
-    name: { type: String, maxlength: 50 },
-    amount: { type: Number, max: 1000 },
-    unit: { 
+  // Wrapper fields for easier destructuring in FE/organization
+  content: {
+    title: { 
       type: String, 
-      default: 'oz' 
-    }
-  }]
+      required: true,
+      maxlength: 80
+    },
+    description: { type: String },
+    imageUrl: { type: String },
+    externalLink: { type: String },
+    ingredients: [{
+      name: { type: String, maxlength: 50 },
+      amount: { type: Number, min: 0, max: 1000 },
+      unit: { type: String, default: 'oz' }
+    }],
+    instructions: { type: [String], maxlength: 200 }
+  },
+
+  interactions: {
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    comments: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }]
+  },
 }, { timestamps: true });
 
 // Compile Schema into a Model and export
